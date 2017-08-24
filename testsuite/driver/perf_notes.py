b9e3527744ff5783d6978683f763013ac5c7297f
Jared Weakly <jweakly@pdx.edu>
2017-08-24 14:24:36 -0700

    Fix importing issue in testsuite driver

--- testsuite/driver/perf_notes.py
+++ testsuite/driver/perf_notes.py
@@ -12,107 +12,88 @@ import argparse
 import re
 import subprocess
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--test-env",
-                    help="The given test environment to be compared.")
-parser.add_argument("--test-name",
-                    help="Optional: If given, filters table to include only \
-                    tests matching the given regular expression.")
-parser.add_argument("--min-delta",type=float,
-                    help="Optional: Display only tests where the relative \
-                    spread is greater than the given value. \
-                    This will not be run if you only pass in one commit.")
-parser.add_argument("--add-note", nargs=3,
-                    help="Development only. Adds N fake metrics to the given commit. \
-                    If the third argument is not a blank string, this will generate \
-                    different looking fake metrics.")
-parser.add_argument("commits", nargs=argparse.REMAINDER,
-                    help="The rest of the arguments will be the commits that will be used.")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test-env",
+                        help="The given test environment to be compared.")
+    parser.add_argument("--test-name",
+                        help="Optional: If given, filters table to include only \
+                        tests matching the given regular expression.")
+    parser.add_argument("--min-delta",type=float,
+                        help="Optional: Display only tests where the relative \
+                        spread is greater than the given value. \
+                        This will not be run if you only pass in one commit.")
+    parser.add_argument("--add-note", nargs=3,
+                        help="Development only. Adds N fake metrics to the given commit. \
+                        If the third argument is not a blank string, this will generate \
+                        different looking fake metrics.")
+    parser.add_argument("commits", nargs=argparse.REMAINDER,
+                        help="The rest of the arguments will be the commits that will be used.")
+    args = parser.parse_args()
 
-#
-# Defaults and utilities
-#
+    #
+    # Defaults and utilities
+    #
 
-env = 'local'
-name = re.compile('.*')
-# metrics is a dictionary of the form
-# [ {'test_env': 'local', 'test': 'T100', 'way': 'some_way', 'metric': 'some_field', 'value': '1000', 'commit': 'HEAD'} ]
-metrics = []
-singleton_commit = len(args.commits) == 1
+    env = 'local'
+    name = re.compile('.*')
+    # metrics is a dictionary of the form
+    # [ {'test_env': 'local', 'test': 'T100', 'way': 'some_way', 'metric': 'some_field', 'value': '1000', 'commit': 'HEAD'} ]
+    metrics = []
+    singleton_commit = len(args.commits) == 1
 
+    #
+    # Main logic of program
+    #
 
-# This function allows one to read in git notes from the commandline
-# and then breaks it into a list of dictionaries that can be parsed
-# later on in the testing functions.
-# Silently returns an empty string if the note is not found.
-def parse_git_notes(namespace, commit='HEAD'):
-    logFields = ['test_env','test','way','metric','value','commit']
+    if args.commits:
+        for c in args.commits:
+            metrics += parse_git_notes('perf',c)
 
-    try:
-        log = subprocess.check_output(['git', 'notes', '--ref=' + namespace, 'show', commit], stderr=subprocess.STDOUT).decode('utf-8')
-    except subprocess.CalledProcessError:
-        return []
+    if args.test_env:
+        metrics = [test for test in metrics if test['test_env'] == args.test_env]
 
-    log = log.strip('\n').split('\n')
-    log = list(filter(None, log))
-    log = [line.strip('\t').split('\t') for line in log]
-    [x.append(commit) for x in log]
-    log = [dict(zip(logFields, field)) for field in log]
-    return log
+    if args.test_name:
+        name = re.compile(args.test_name)
+        metrics = [test for test in metrics if name.search(test.get('test',''))]
 
-#
-# Main logic of program
-#
+    if args.min_delta:
+        delta = args.min_delta
 
-if args.commits:
-    for c in args.commits:
-        metrics += parse_git_notes('perf',c)
+        def cmp(v1, v2):
+            if v1 > v2:
+                return (100 * (v1 - v2)/v2) > delta
+            else:
+                return (100 * (v2 - v1)/v1) > delta
 
-if args.test_env:
-    metrics = [test for test in metrics if test['test_env'] == args.test_env]
+        m = []
+        for t in latest_commit:
+            m += [(t,test) for test in metrics if (t['test'] == test['test']) and (t['commit'] != test['commit'])]
 
-if args.test_name:
-    name = re.compile(args.test_name)
-    metrics = [test for test in metrics if name.search(test.get('test',''))]
+        deltas = []
+        for fst,snd in m:
+            if cmp(float(fst['value']),float(snd['value'])):
+                deltas.append(fst)
 
-if args.min_delta:
-    delta = args.min_delta
+        # Throw away the work if we only have one commit passed in.
+        # Ugly way to do it but ¯\_(ツ)_/¯
+        if not singleton_list:
+            metrics = deltas
 
-    def cmp(v1, v2):
-        if v1 > v2:
-            return (100 * (v1 - v2)/v2) > delta
-        else:
-            return (100 * (v2 - v1)/v1) > delta
+    if args.add_note:
+        def note_gen(n, commit, delta=''):
+            note = []
+            # Generates simple fake data. Likely comprehensive enough to catch all edge cases.
+            if not delta:
+                [note.append('\t'.join(['local', 'T'+ str(i*100), 'some_way', 'some_field', str(i*1000)])) for i in range(1,int(int(n)/2)+1)]
+                [note.append('\t'.join(['non-local', 'W'+ str(i*100), 'other_way', 'other_field', str(i*100)])) for i in range(int(int(n)/2)+1,int(n)+1)]
+            if delta:
+                [note.append('\t'.join(['local', 'T'+ str(i*100), 'some_way', 'some_field', str(i*10)])) for i in range(1,int(int(n)/2)+1)]
+                [note.append('\t'.join(['non-local', 'W'+ str(i*100), 'other_way', 'other_field', str(i*1)])) for i in range(int(int(n)/2)+1,int(n)+1)]
 
-    m = []
-    for t in latest_commit:
-        m += [(t,test) for test in metrics if (t['test'] == test['test']) and (t['commit'] != test['commit'])]
+            git_note = subprocess.check_output(["git","notes","--ref=perf","append",commit,"-m", "\n".join(note)])
 
-    deltas = []
-    for fst,snd in m:
-        if cmp(float(fst['value']),float(snd['value'])):
-            deltas.append(fst)
-
-    # Throw away the work if we only have one commit passed in.
-    # Ugly way to do it but ¯\_(ツ)_/¯
-    if not singleton_list:
-        metrics = deltas
-
-if args.add_note:
-    def note_gen(n, commit, delta=''):
-        note = []
-        # Generates simple fake data. Likely comprehensive enough to catch all edge cases.
-        if not delta:
-            [note.append('\t'.join(['local', 'T'+ str(i*100), 'some_way', 'some_field', str(i*1000)])) for i in range(1,int(int(n)/2)+1)]
-            [note.append('\t'.join(['non-local', 'W'+ str(i*100), 'other_way', 'other_field', str(i*100)])) for i in range(int(int(n)/2)+1,int(n)+1)]
-        if delta:
-            [note.append('\t'.join(['local', 'T'+ str(i*100), 'some_way', 'some_field', str(i*10)])) for i in range(1,int(int(n)/2)+1)]
-            [note.append('\t'.join(['non-local', 'W'+ str(i*100), 'other_way', 'other_field', str(i*1)])) for i in range(int(int(n)/2)+1,int(n)+1)]
-
-        git_note = subprocess.check_output(["git","notes","--ref=perf","append",commit,"-m", "\n".join(note)])
-
-    note_gen(args.add_note[0],args.add_note[1],args.add_note[2])
+        note_gen(args.add_note[0],args.add_note[1],args.add_note[2])
 
 #
 # Comparison tools for commits.
@@ -204,12 +185,12 @@ def evaluate_metric(test, field, deviation, contents):
 #
 # String utilities for pretty-printing
 #
-
-string = ''
-for i in args.commits:
-    string+='{:18}'
-commits = string.format(*[c[:10] for c in args.commits])
-latest_commit = [test for test in metrics if test['commit'] == args.commits[0]]
+def main_2():
+    string = ''
+    for i in args.commits:
+        string+='{:18}'
+    commits = string.format(*[c[:10] for c in args.commits])
+    latest_commit = [test for test in metrics if test['commit'] == args.commits[0]]
 
 def cmtline(insert):
     return string.format(*[insert for c in args.commits]).strip()
@@ -247,15 +228,21 @@ def commit_string(test, flag):
 # The pretty-printed output
 #
 
-header('commit')
-# Printing out metrics.
-for test in latest_commit:
-    print("{:27}{:30}".format(test['test'], test['metric']) + commit_string(test['test'],'metrics'))
-
-# Has no meaningful output if there is no commit to compare to.
-if not singleton_commit:
-    header('percent')
-
-    # Printing out percentages.
+def main_3():
+    header('commit')
+    # Printing out metrics.
     for test in latest_commit:
-        print("{:27}{:30}".format(test['test'], test['metric']) + commit_string(test['test'],'percentages'))
+        print("{:27}{:30}".format(test['test'], test['metric']) + commit_string(test['test'],'metrics'))
+
+    # Has no meaningful output if there is no commit to compare to.
+    if not singleton_commit:
+        header('percent')
+
+        # Printing out percentages.
+        for test in latest_commit:
+            print("{:27}{:30}".format(test['test'], test['metric']) + commit_string(test['test'],'percentages'))
+
+if __name__ == '__main__':
+    main()
+    main_2()
+    main_3()
